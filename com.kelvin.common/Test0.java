357f9ee948ceaaf725c0be5f072c58596446925a
tangshulei <tangshulei@360jinrong.net>
2019-12-19 11:48:34 +0800

    1.0.0

---
+++ com.kelvin.common/Test0.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName Test0
+ * @Descripiton
+ * @Author kelvin
+ * @Date 2019/12/19 上午11:48
+ * @Version 1.0
+ **/
+public class Test0 {
+
+    public static void main(String[] args) {
+        System.out.println("1.0.0");
+    }
+}
